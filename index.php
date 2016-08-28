df13448d35edec3ffa10c0894ba26ad5b5b948d4
Reaal Khalil <reaal@reaal.me>
2016-08-28 14:44:18 +0100

    cleaned up a bit

--- index.php
+++ index.php
@@ -1,32 +1,20 @@
 <?php 
+	if($_GET["day"]){
+		$day=$_GET["day"];
+		$curl = curl_init('http://apod.nasa.gov/apod/ap'.$day.'.html');
+	}else{
+		$curl = curl_init('http://apod.nasa.gov/apod/astropix.html');
+	}
 
-//$curl = curl_init('http://apod.nasa.gov/apod/astropix.html');
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	$result = curl_exec($curl);
+	$dom = new DomDocument();
+	@$dom->loadHTML($result);
 
-if($_GET["day"]){
-$day=$_GET["day"];
-$curl = curl_init('http://apod.nasa.gov/apod/ap'.$day.'.html');
-}else{
-$curl = curl_init('http://apod.nasa.gov/apod/astropix.html');
-}
-
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-$result = curl_exec($curl);
-$dom = new DomDocument();
-@$dom->loadHTML($result);
-
-foreach ($dom->getElementsByTagName('img') as $img) {
+	$img = $dom->getElementsByTagName('img')->item(0);
 	$path="http://apod.nasa.gov/apod/".$img->getAttribute('src');
-}
 
-if($path!=''){
-	header("Location: ".$path);
-	//header("Location: http://apod.nasa.gov/apod/".$img->getAttribute('src'));
-	//fileExists($path);
-}else{
-
-if($day!=''){header("Location: bgvideo.php?day=".$day);}else{
-header("Location: bgvideo.php");
-}
-
-}
+	if($path!=''){
+		header("Location: ".$path);
+	}
 ?>
